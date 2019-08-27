e2ff588c4e85a5d68d0853a18688a1bd619a9abc
hatanao <onepiece.love.708@gmail.com>
2019-08-27 17:08:11 +0800

    remove subtitle from welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app')
 @section('content')
-    <div class="banner">
-                <img class="w-100" src={{ asset('img/image_2019_7_29.png') }} alt="">
+        <div class="banner p-o">
+                <img class="w-100 mb-3" src={{ asset('img/image_2019_7_29.png') }} alt="">
                 <h1 class="page-title text-center">Awesome Blog App</h1>
         </div>
-        <div class="col-md-12">
-                <div class="col-md-6 mx-auto"><p>Welcome to by blog~!</p></div>
-                </div>
-    @endsection
+@endsection
